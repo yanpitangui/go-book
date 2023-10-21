b303daf6653db22d6c3bce142cb0412830524382
Yan Pitangui <yanpitangui@gmail.com>
2023-10-20 21:46:25 -0300

    Reading files

---
+++ reading_files/blogposts_test.go
@@ -0,0 +1,73 @@
+package blogposts_test
+
+import (
+	"errors"
+	blogposts "go-book/reading_files"
+	"io/fs"
+	"reflect"
+	"testing"
+	"testing/fstest"
+)
+
+func TestNewBlogPosts(t *testing.T) {
+	const (
+		firstBody = `Title: Post 1
+Description: Description 1
+Tags: tdd, go
+---
+Hello
+World`
+		secondBody = `Title: Post 2
+Description: Description 2
+Tags: rust, borrow-checker
+---
+B
+L
+M`
+	)
+
+	fs := fstest.MapFS{
+		"hello world.md":  {Data: []byte(firstBody)},
+		"hello-world2.md": {Data: []byte(secondBody)},
+	}
+
+	posts, err := blogposts.NewPostsFromFS(fs)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assertPost(t, posts[0], blogposts.Post{
+		Title:       "Post 1",
+		Description: "Description 1",
+		Tags:        []string{"tdd", "go"},
+		Body: `Hello
+World`,
+	})
+
+	if len(posts) != len(fs) {
+		t.Errorf("got %d posts, wanted %d posts", len(posts), len(fs))
+	}
+}
+
+func TestFailingFs(t *testing.T) {
+	_, err := blogposts.NewPostsFromFS(StubFailingFS{})
+
+	if err == nil {
+		t.Fatal("wanted an error but didn't get one")
+	}
+}
+
+type StubFailingFS struct {
+}
+
+func (s StubFailingFS) Open(name string) (fs.File, error) {
+	return nil, errors.New("oh no, i always fail")
+}
+
+func assertPost(t *testing.T, got blogposts.Post, want blogposts.Post) {
+	t.Helper()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %+v, want %+v", got, want)
+	}
+}
---
+++ reading_files/cmd/posts/hello world.md
@@ -0,0 +1,7 @@
+Title: Hello, TDD world!
+Description: First post on our wonderful blog
+Tags: tdd, go
+---
+Hello world!
+
+The body of posts starts after the `---` line. The first line is the title, the second is the description and the third is the tags. The body of the post is the rest of the file.
